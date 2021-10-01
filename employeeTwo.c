6859b64143689e5b0c50c9c723a791b80b43b003
Kristine Trevino Kinoshita <kristinet@cpp.edu>
2021-09-30 19:35:15 -0700

    Updated employeeTwo.c file to include two new search functions to find if an employee has a certain phone number and salary

--- employeeTwo.c
+++ employeeTwo.c
@@ -28,6 +28,14 @@ static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableVa
 {
     return strcmp((char *) targetPtr, tableValuePtr->name); //const void *targetPtr ==> typecast asa char pointer then pass into strcmp()
 }
+static int compareEmployeePhone(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return strcmp((char *) targetPtr, tableValuePtr->phone);
+}
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return * (double *) targetPtr != tableValuePtr->salary;
+}
 
 //These are called wrappers. These functions are what you will use in your main!!!
 PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number)
@@ -37,4 +45,12 @@ PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long numb
 PtrToEmployee searchEmployeeByName(PtrToEmployee ptr, int size, char* name)
 {
     return searchEmployeeTable(ptr, size, name, compareEmployeeName);
+}
+PtrToEmployee searchEmployeeByPhone(PtrToEmployee ptr, int size, char* phone)
+{
+    return searchEmployeeTable(ptr, size, phone, compareEmployeePhone);
+}
+PtrToEmployee searchEmployeeBySalary(PtrToEmployee ptr, int size, double salary)
+{
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeSalary);
 }
